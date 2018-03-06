1b7560ed3bcb246196facf267f629cf0164988c2
eevav <eevavilson@gmail.com>
2018-03-06 02:01:16 +0200

    Initial commit

---
+++ Level.gd
@@ -0,0 +1,9 @@
+extends Node
+
+var nuts_total = 0
+var nuts_collected = 0
+
+func _ready():
+
+	pass
+
---
+++ nuts.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+export var value = 1
+
+func _ready():
+	print(get_owner().get_name())
+	if get_owner() != null:
+		get_owner().nuts_total += value
+	
+	get_node("Area2D").connect("body_enter",self,"_collect_peanut")
+
+
+
+
+func _collect_peanut( body ):
+	print(get_owner().nuts_collected)
+	if get_owner() != null:
+		get_owner().nuts_collected += value
+	print(get_owner().nuts_collected)	
+	queue_free()
